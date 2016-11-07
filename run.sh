6738cf88b3409ebc6c71353aed592d3ad581ea57
Jarkko Miettinen <jarkko.miettinen@relex.fi>
2016-11-07 15:00:38 +0200

    Better explanation in the runner script on what's going on

--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@ mvn clean package
 
 echo "--- This won't work properly as there's no JRuby in boot classpath"
 java -jar target/jruby-test.jar src/main/ruby/test.rb
-
+if [ $? -ne 0 ]; then
+    echo "--- Failed as expected"
+else
+    echo "--- Worked unexpectedly"
+fi
 echo "--- But if we put the JRuby jar into both classpath, things work"
 java -Xbootclasspath/a:`mvn dependency:build-classpath|grep -E ".*\.jar"` -classpath target/jruby-test.jar org.jruby.Main src/main/ruby/test.rb
+if [ $? -ne 0 ]; then
+    echo "--- Failed unexpectedly"
+else
+    echo "--- Worked as expected"
+fi
